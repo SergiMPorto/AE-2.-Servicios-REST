e82c464810eb3a5d5865cd9984f7d84e4bb32806
Sergi <121488207+SergiMPorto@users.noreply.github.com>
2023-11-14 10:17:47 +0100

    agregando metodos

--- A2Rest/src/main/java/es/SistemasRest/persistencia/DaolLibro.java
+++ A2Rest/src/main/java/es/SistemasRest/persistencia/DaolLibro.java
@@ -5,22 +5,51 @@ import java.util.List;
 
 import es.SistemasRest.entidad.libro;
 
+
 public class DaolLibro {
 
-	public static List<libro> listaLibros;
-	
-	static {
-		listaLibros = new ArrayList<libro>();
-		
-		libro l1 = new libro(01, "La Vida Sexual de Catherine MIller","Catherine Miller", "Galerista de arte que narra sus experiencias sexuales");
-	    libro l2 = new libro(01,"Corazón tan blanco", "Javier Marías", "Primer éxito internacional de Javiar Marías");
-		libro l3 = new libro(03, "American Psycho", "Breat Easton Ellis", "Las hazañas de Patrick Bateman ");
-		libro l4 = new libro(04, "La mancha Humana", "Philip Roth", "Philip Roth se adelantó a la generacion Woke");
-		libro l5 = new libro(05, "2666", "Roberto Bolaño", "Obra monumental de la literatura latinoamericana");
-		listaLibros.add(l1);
-		listaLibros.add(l2);
-		listaLibros.add(l3);
-		listaLibros.add(l4);
-		listaLibros.add(l5);
-	}
+    public static List<libro> listaLibros;
+
+    static {
+        listaLibros = new ArrayList<libro>();
+
+        libro l1 = new libro(01, "La Vida Sexual de Catherine MIller","Catherine Miller", "Galerista de arte que narra sus experiencias sexuales");
+        libro l2 = new libro(01,"Corazón tan blanco", "Javier Marías", "Primer éxito internacional de Javier Marías");
+        libro l3 = new libro(03, "American Psycho", "Breat Easton Ellis", "Las hazañas de Patrick Bateman ");
+        libro l4 = new libro(04, "La mancha Humana", "Philip Roth", "Philip Roth se adelantó a la generacion Woke");
+        libro l5 = new libro(05, "2666", "Roberto Bolaño", "Obra monumental de la literatura latinoamericana");
+        listaLibros.add(l1);
+        listaLibros.add(l2);
+        listaLibros.add(l3);
+        listaLibros.add(l4);
+        listaLibros.add(l5);
+    }
+
+    public libro get(int id) {
+        return listaLibros.get(id);
+    }
+
+    public List<libro> list() {
+        return listaLibros;
+    }
+
+    public void añadirLibro(libro l) {
+        boolean existeLibros = false;
+
+        for (libro existeLibro : listaLibros) {
+            if (existeLibro.getId() == l.getId() || existeLibro.getTitulo().equals(l.getTitulo())) {
+                existeLibros = true;
+                break;
+            }
+        }
+
+        if (!existeLibros) {
+            listaLibros.add(l);
+        }
+    }
+
+
+    public libro delete(int id) {
+        return listaLibros.remove(id);
+    }
 }
